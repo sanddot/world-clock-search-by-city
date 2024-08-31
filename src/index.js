371f61f4ac202ab6cd493ae7e85d18c309bdb6fd
sanddot <sanginta@gmail.com>
2024-08-31 14:38:25 -0700

    Added location in HTML and data function for it in java script

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ function updateTime() {
     let losAngelesTimeElement = losAngelesElement.querySelector(".time");
     let losAngelesTimeData = moment().tz("America/Los_Angeles");
 
-    losAngelesDateElement.innerHTML = losAngelesTimeData.format("MMMM Do YYYY");
+    losAngelesDateElement.innerHTML = losAngelesTimeData.format(
+      "dddd, MMMM Do, YYYY"
+    );
     losAngelesTimeElement.innerHTML = losAngelesTimeData.format(
       "h:mm:ss [<small>]A[</small>]"
     );
@@ -16,8 +18,23 @@ function updateTime() {
     let torontoTimeElement = torontoElement.querySelector(".time");
     let torontoTimeData = moment().tz("America/Toronto");
 
-    torontoDateElement.innerHTML = torontoTimeData.format("MMMM Do YYYY");
+    torontoDateElement.innerHTML = torontoTimeData.format(
+      "dddd, MMMM Do, YYYY"
+    );
     torontoTimeElement.innerHTML = torontoTimeData.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
+  let amsterdamElement = document.querySelector("#amsterdam");
+  if (amsterdamElement) {
+    let amsterdamDateElement = amsterdamElement.querySelector(".date");
+    let amsterdamTimeElement = amsterdamElement.querySelector(".time");
+    let amsterdamTimeData = moment().tz("Europe/Amsterdam");
+
+    amsterdamDateElement.innerHTML = amsterdamTimeData.format(
+      "dddd, MMMM Do, YYYY"
+    );
+    amsterdamTimeElement.innerHTML = amsterdamTimeData.format(
       "h:mm:ss [<small>]A[</small>]"
     );
   }
@@ -34,13 +51,15 @@ function updateCity(event) {
    <div class="city">
       <div>
         <h2>${cityName}</h2>
-        <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+        <div class="date">${cityTime.format("dddd, MMMM Do, YYYY")}</div>
       </div>
       <div class="time">${cityTime.format("h:mm:ss")}<small> ${cityTime.format(
     "A"
   )}</small>
       </div>
    </div>
+   </br>
+   <a href="/"><small>Back to all cities</small></a>
  `;
 }
 
